d00cd6fbafc3a2c95eb4c8245617fa2d326a105c
stupendous-trainer-5541 <peter.c.viss@biola.edu>
2019-02-26 01:56:51 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -17,7 +17,7 @@ end
 
 def pop_with_args(chars_in_game_of_thrones)
   @chars_in_game_of_thrones = ["Danny T.", "Tyrion Lannister", "Stable Boy", "Sandor Clegane, aka The Mountain"]
-@chars_arya_killed =  @chars_in_game_of_thrones.pop(1)
+@chars_arya_killed =  @chars_in_game_of_thrones.pop(-1)
   return @chars_arya_killed
 end
 
